7dcbdf4202772191838d1894716c0e07d62af8b4
reggiegfl <reggiegfl@yahoo.com>
2018-06-09 21:44:34 -0400

    Update crystal game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,7 +14,7 @@ console.log("num 1 = ", num1, "num 2 = ", num2, "num 3 = ", num3, "num 4 = ", nu
 // displays the number to be achieved to the page
 $("#goal").text(target);
 
-//establishing initial variables
+//establishing initial global variables
 var wins = 0;
 var losses = 0;
 var yourTotal = 0;
@@ -34,78 +34,71 @@ function winner(){
 
 }
 
-//function adding wins to win total
+//function adding losses to loss total
 function loser(){
     alert ("You Lost!");
     losses++;
     $("#losses").text(losses);
-    console.log("New loss total: " , losses);
-    
+    console.log("New loss total: " , losses);    
+}
+function checkWinLoss (num){
+    console.log(num);
+    yourTotal = yourTotal + num;
+    console.log("New userTotal= " + yourTotal);
+    //add number from click to your current total
+    $("#score").text(yourTotal);
+    if (yourTotal == target){
+        winner();
+        resetGame ();    
+
+    }
+    else if (yourTotal > target){
+        loser();
+        resetGame ();    
+
+    }
 }
 
 //sets up click for jewel 1
 $('#gemOne').on ('click', function(){
-    yourTotal = yourTotal + num1;
-    console.log("New userTotal= " + yourTotal);
-//add number from click to your current total
-$("#score").text(yourTotal);
-
-    if (yourTotal == target){
-        winner();
-    }
-    else if (yourTotal > target){
-        loser();
-    }
+    checkWinLoss(num1);   
 })       
-
 
 //sets up click for jewel 2
 $('#gemTwo').on ('click', function(){
-    yourTotal = yourTotal + num2;
-    console.log("New userTotal= " + yourTotal);
-//add number from click to your current total
-$("#score").text(yourTotal);
-
-if (yourTotal == target){
-    winner();
-}
-else if (yourTotal > target){
-    loser();
-}
+    checkWinLoss(num2);
 })
 
 //sets up click for jewel 3
 $('#gemThree').on ('click', function(){
-    yourTotal = yourTotal + num3;
-    console.log("New userTotal= " + yourTotal);
-//add number from click to your current total
-$("#score").text(yourTotal);
-
-if (yourTotal == target){
-    winner();
-}
-else if (yourTotal > target){
-    loser();
-}
+    checkWinLoss(num3);
 })
 
 //sets up click for jewel 4
 $('#gemFour').on ('click', function(){
-    yourTotal = yourTotal + num4;
-    console.log("New userTotal= " + yourTotal);
-//add number from click to your current total
-$("#score").text(yourTotal);
-
-if (yourTotal == target){
-    winner();
-}
-else if (yourTotal > target){
-    loser();
-}
+    checkWinLoss(num4);
 })
 
-//win conditions
+//reset game function defined
+function resetGame (){
+    yourTotal = 0;
+    target = Math.floor(Math.random() *101) + 19; 
+    $("#score").text(yourTotal);
+    console.log (target, "Your Total is: ", yourTotal);
+    
+    // Setting up random numbers for each jewel
+    num1= Math.floor(Math.random()*11+1);
+    num2= Math.floor(Math.random()*11+1);
+    num3= Math.floor(Math.random()*11+1);
+    num4= Math.floor(Math.random()*11+1);
+    console.log("num 1 = ", num1, "num 2 = ", num2, "num 3 = ", num3, "num 4 = ", num4);
+    
+    // displays the number to be achieved to the page
+    $("#goal").text(target);
 
+
+
+}
 
 
 
